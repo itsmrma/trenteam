e375dc2faffa469505b3220e3885e172dd59642c
itsmrma <m.benassi30@gmail.com>
2025-02-19 18:51:43 +0100

    upd

--- hackaton/myapp/views.py
+++ hackaton/myapp/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        'name' : 'Matteo Benassi',
-        'car' : 'Renault Clio',
+        'name' : 'Hackaton',
         'age' : 10,
     }
     return render(request, 'home.html', context)
