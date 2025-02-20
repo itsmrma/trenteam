f43b3981890105ca90aa9be52aac9a51043c5f83
Stortplex5 <94644956+Stortplex5@users.noreply.github.com>
2025-02-20 11:06:17 +0100

    urls.py modificato

--- hackaton/hackaton/urls.py
+++ hackaton/hackaton/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.upload_file, name='upload_file')
+    path('', views.upload_file, name='upload_file'),
+    path('result',views.upload_file,name='result')
 ]
